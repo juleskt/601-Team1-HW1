663ab85b70e30d5822f6358dd1e07d45b4b743cc
Julian Trinh <jtrinh@vmware.com>
2017-09-10 17:10:05 -0400

    Modified https_template to be vertical

--- httpstat/httpstat.py
+++ httpstat/httpstat.py
@@ -63,14 +63,26 @@ curl_format = """{
 }"""
 
 https_template = """
-  DNS Lookup   TCP Connection   TLS Handshake   Server Processing   Content Transfer
-[   {a0000}  |     {a0001}    |    {a0002}    |      {a0003}      |      {a0004}     ]
-             |                |               |                   |                  |
-    namelookup:{b0000}        |               |                   |                  |
-                        connect:{b0001}       |                   |                  |
-                                    pretransfer:{b0002}           |                  |
-                                                      starttransfer:{b0003}          |
-                                                                                 total:{b0004}
+{   DNS Lookup            }
+|   {a0000}               |
+|   namelookup:{b0000}    |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{   TCP Connection        }
+|   {a0001}               |
+|   connect:{b0001}       |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{   TLS Handshake         }
+|   {a0002}               |
+|   pretransfer:{b0002}   |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{   Server Processing     }
+|   {a0003}               |
+|   starttransfer:{b0003} |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{  Content Transfer       }
+|   {a0004}               |
+|   total:{b0004}         |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """[1:]
 
 http_template = """
